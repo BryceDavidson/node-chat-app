b1cf6fa4379981d7e7fcece756f56b3c3604c0f2
Bryce Davidson <lifebryce@gmail.com>
2018-01-14 11:07:14 -0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, "../public");
+var app = express();
+
+app.use(express.static(publicPath));
+
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Server has started on port:', port)
+});
